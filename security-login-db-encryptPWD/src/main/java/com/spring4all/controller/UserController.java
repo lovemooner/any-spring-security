4834a72aaecc54bcae58420c524679cbbc761740
lovemooner <lovemooner@yeah.net>
2019-02-15 17:54:22 +0800

    ttt

--- security-login-db-encryptPWD/src/main/java/com/spring4all/controller/UserController.java
+++ security-login-db-encryptPWD/src/main/java/com/spring4all/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.spring4all.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +15,24 @@ import java.security.Principal;
 public class UserController {
 
     @GetMapping("/user")
-    public String user(@AuthenticationPrincipal Principal principal, Model model){
+    public String user(@AuthenticationPrincipal Principal principal, Model model) {
+
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+
+// 获取当事人信息对象，返回结果是 Object 类型，但实际上可以是应用程序自定义的带有更多应用相关信息的某个类型。
+// 很多情况下，该对象是 Spring Security 核心接口 UserDetails 的一个实现类，你可以把 UserDetails 想像
+// 成我们数据库中保存的一个用户信息到 SecurityContextHolder 中 Spring Security 需要的用户信息格式的
+// 一个适配器。
+        Object principal1 = authentication.getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails) principal).getUsername();
+        } else {
+            String username = principal.toString();
+        }
+
+
         model.addAttribute("username", principal.getName());
         return "user/user";
     }
